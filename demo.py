8da514b2822e2d30d800236f675b0388e68d9494
allstartcc <274259167@qq.com>
2020-03-24 23:13:20 +0800

    day2

--- demo.py
+++ demo.py
@@ -8,7 +8,7 @@
 # b = (a,"mm","nn","bb")
 # print(b[0][4]) #二维元组
 
-c = [1,4,5,6,7,"q","ww"]
+# c = [1,4,5,6,7,"q","ww"]
 # c.append("haha") #在元组末尾添加数据
 # c.insert(2,"jaj")
 # print(c)
@@ -20,10 +20,35 @@ c = [1,4,5,6,7,"q","ww"]
 # xx = ["nihao","buaho"]
 # print(c+xx)
 # print(c)
-a = c[3]
-print(a)
+# a = c[3]
+# print(a)
 """
 所有的方法都是小括号结尾 print  input insert len
-元组,数组,字典的取值都用中括号
+元组,数组,字典的[取值]都用中括号
  ()   {}   {}
 """
+"""
+字典的特点
+1.字典中的值没有顺序
+2.字典的接口必须是键值对 key:value
+"""
+a = {"name":"cc"}
+#取值
+print(a["name"])
+#新增
+a["high"] = "189cm"
+print(a)
+#修改
+a["name"] = "gihub"
+print(a)
+
+b = a.get("name")
+print(b)
+c = a.pop("name")
+print(c)
+a.update(name=1111)
+print(a)
+#数组和字典的删除'
+del a["name"]
+a.update(nam = "aaa")
+print(a)
---
+++ demo1.py
@@ -0,0 +1,5 @@
+a = {}
+a.update(name = str(input("请输入name:")))
+a.update(age = int(input("请输入age:")))
+a.update(sex = input("请输入sex:"))
+print(a)
