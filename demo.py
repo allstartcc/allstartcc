0508a14b8356341144cecbc778719ec5ec3d3846
allstartcc <274259167@qq.com>
2020-03-24 21:58:23 +0800

    gitest

--- demo.py
+++ demo.py
@@ -1,7 +1,29 @@
-a = ("呵呵","aa","aa","qq","ee","tt","hh")
-print(a[3])
-print(a.index("hh"))
-print(a.count("aa"))
-print(a[3:4]) #切片
-b = (a,"mm","nn","bb")
-print(b[0][3]) #二元维组
+# a = ("呵呵","aa","aa","qq","ee","tt","hh")
+# print(a[3])
+# print(a.index("hh")) # 位数
+# print(a.count("aa")) # 统计个数
+# print(a[3:4]) # 切片
+# print(a[:4]) # 切片 左开右闭 0到4
+# print(a[3:]) # 切片 左开右闭 3到最后
+# b = (a,"mm","nn","bb")
+# print(b[0][4]) #二维元组
+
+c = [1,4,5,6,7,"q","ww"]
+# c.append("haha") #在元组末尾添加数据
+# c.insert(2,"jaj")
+# print(c)
+# b = c.pop(0) #剪切 将数组的数据拿出来用
+# d = c.pop(0) 
+# print(c)
+# print(b)
+# print(d)
+# xx = ["nihao","buaho"]
+# print(c+xx)
+# print(c)
+a = c[3]
+print(a)
+"""
+所有的方法都是小括号结尾 print  input insert len
+元组,数组,字典的取值都用中括号
+ ()   {}   {}
+"""
